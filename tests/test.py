c0236d3bdcf22be4a5c44eb888c930f3bd2c1424
Kevin Russel <kzone159@gmail.com>
2026-02-08 13:26:29 +0530

    sorting the array

--- tests/test.py
+++ tests/test.py
@@ -10,12 +10,12 @@ def generate_random_array(size,low=0,high=1000):
 
 
 def first_test():
-    size_of_arr = [10,50,5,600,7]
-
+    size_of_arr = [10]
     for key,value in enumerate(size_of_arr):
+        print(value)
         arr = generate_random_array(size=value)
-        print(f'test {key}')
-
+        arr.sort()
+        print(arr)
 
 
 if __name__ == "__main__":
