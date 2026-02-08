8c5f826acd3bd5cc5c1bd84406fd0e79bdceab4a
Kevin Russel <kzone159@gmail.com>
2026-02-08 13:17:17 +0530

    added test one

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,22 @@
 import random
 
-def generate_array(size,low=0,high=1000):
+def generate_random_array(size,low=0,high=1000):
     return [random.randint(low,high) for _ in range(size)]
 
 
 
-size = 10
 
-new_arr = generate_array(size = size)
 
-for value in new_arr:
-    print(value)
+
+
+def first_test():
+    size_of_arr = [10,50,5,600,7]
+
+    for key,value in enumerate(size_of_arr):
+        arr = generate_random_array(size=value)
+        print(f'test {key}')
+
+
+
+if __name__ == "__main__":
+    first_test()
