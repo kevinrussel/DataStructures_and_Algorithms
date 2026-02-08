3e7b77db8e4b7360f3d0b5714d96f1506eb71b8f
Kevin Russel <kzone159@gmail.com>
2026-02-08 15:15:23 +0530

    finished linear and binary search with tests

--- Algorithms/serach_algorithms.py
+++ Algorithms/serach_algorithms.py
@@ -8,4 +8,15 @@ def linear_search(arr,target):
     return -1
 
 def binary_search(arr,target):
-    pass
+    left = 0
+    right = len(arr)-1
+    while(not left>right):
+        mid = (left + right)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            right = mid -1
+        else:
+            left = mid + 1
+    return -1
+    
